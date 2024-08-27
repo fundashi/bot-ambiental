688f7f27dd12497285a00d65aed7822d4345021e
christofer <christofer.rios.garcia@gmail.com>
2024-08-27 14:29:50 -0500

    Add files via upload

---
+++ botambiental.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+
+intens = discord.Intents.default()
+intens.message_content = True
+
+bot=commands.Bot(command_prefix = "!", intents = intens)
+
+@bot.event
+async def on_ready():
+    print(f"se inicio el bot{bot.user}")
+
+
+descomposicion_residuos = {
+    "botella de plastico":500,
+    "lata de aluminio":40,
+    "bolsa de plastico":150,
+    "vidrio":4000
+}
+
+@bot.command()
+async def impacto(ctx,objeto:str):
+    objeto = objeto.lower()
+    if objeto in descomposicion_residuos:
+        tiempo = descomposicion_residuos[objeto]
+        await ctx.send(f"El objeto {objeto} tarda aproximadamente {tiempo} años")
+
+        if tiempo >= 100:
+            await ctx.send("Porfavor reutilizar el producto o utilizar otro producto.")
+    else:
+        await ctx.send("No tenemos información de {objeto}")
+
+
+bot.run("token")
